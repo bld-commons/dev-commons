7bcfab9c683f11dc060a108569b70923fd2daffd
Francesco Baldi <francesco.baldi1987@gmail.com>
2023-09-05 11:01:33 +0200

    crypto

---
+++ crypto-example/src/main/java/com/bldcommons/example/controller/AesController.java
@@ -0,0 +1,41 @@
+package com.bldcommons.example.controller;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bldcommons.example.model.EmployeeAes;
+import com.bldcommons.example.model.EmployeePubKey;
+import com.bldcommons.example.model.Registry;
+
+@RestController
+@RequestMapping("/aes")
+public class AesController {
+	
+	private final static Logger logger=LoggerFactory.getLogger(AesController.class);
+
+	@GetMapping(path="/encrypt",produces = "application/json")
+	@ResponseBody
+	public EmployeeAes encryptEmployee() {
+		Registry registry=new Registry("Mario", "Rossi", new Date());
+		List<Integer>idProfile=Arrays.asList(1,2,3);
+		Integer[] idAuthorities=new Integer[] {1,2,3};
+		EmployeeAes employee=new EmployeeAes(registry, idProfile, idAuthorities);
+		return employee;
+	}
+	
+	
+	@PostMapping(path="/decrypt",consumes = "application/json")
+	public void decryptEmploy(@RequestBody EmployeeAes employee) {
+		logger.info(employee.toString());
+	}
+}
---
+++ crypto-example/src/main/java/com/bldcommons/example/model/EmployeeAes.java
@@ -0,0 +1,27 @@
+package com.bldcommons.example.model;
+
+import java.util.List;
+
+import com.bld.crypto.aes.annotation.CryptoAes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class EmployeeAes {
+
+	@CryptoAes("test1")
+	private Registry registry;
+	
+	@CryptoAes("test1")
+	private List<Integer> idProfiles;
+	
+	@CryptoAes("test1")
+	private Integer[] idAuthorities; 
+	
+}
